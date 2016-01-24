ca2d4c135f51c87801a0fca0601115341b44fdaa
zichelle <michelleadean@gmail.com>
2016-01-24 15:35:23 -0600

    Adding Joystick
    Commenting out code involving buttons and adding code for analog
    joystick control of the servos

--- servo_code/servo_code.ino
+++ servo_code/servo_code.ino
@@ -3,14 +3,18 @@
 #include <Servo.h>
 
 //Defining button input pins
-#define UP  7
-#define DOWN  6
-#define LEFT  5
-#define RIGHT 4
+//#define UP  7
+//#define DOWN  6
+//#define LEFT  5
+//#define RIGHT 4
 
 //Creating servo objects
 Servo tilt;
 Servo pan;
+
+//Joystick pins
+const int X_pin = 0; // analog pin connected to X output
+const int Y_pin = 1; // analog pin connected to Y output
 
 int panPos = 90;
 int tiltPos = 90;
@@ -22,10 +26,10 @@ void setup() {
   tilt.attach(8);
 
   //Setting buttons as inputs
-  pinMode(UP, INPUT);
-  pinMode(DOWN, INPUT);
-  pinMode(LEFT, INPUT);
-  pinMode(RIGHT, INPUT);
+  //pinMode(UP, INPUT);
+  //pinMode(DOWN, INPUT);
+  //pinMode(LEFT, INPUT);
+  //pinMode(RIGHT, INPUT);
 
   //Begin servos at center
   pan.write(90);
@@ -34,20 +38,20 @@ void setup() {
 }
 
 //Variables for reading input values
-int upVal;
-int downVal;
-int leftVal;
-int rightVal;
+//int upVal;
+//int downVal;
+//int leftVal;
+//int rightVal;
 
 void loop() {
   //Set direction values
-  upVal=digitalRead(UP);
-  downVal=digitalRead(DOWN);
-  leftVal=digitalRead(LEFT);
-  rightVal=digitalRead(RIGHT);
+  //upVal=digitalRead(UP);
+  //downVal=digitalRead(DOWN);
+  //leftVal=digitalRead(LEFT);
+  //rightVal=digitalRead(RIGHT);
 
   //Check if up button is pressed
-  if (upVal==LOW && tiltPos < 180)
+  if (/*upVal==LOW*/analogRead(Y_pin)<250 && tiltPos < 180)
   {
     tiltPos++;
     tilt.write(tiltPos);
@@ -55,7 +59,7 @@ void loop() {
   }
   
   //or if down button is pressed
-  else if (downVal==LOW && tiltPos > 0)
+  else if (/*downVal==LOW*/analogRead(Y_pin)>450 && tiltPos > 0)
   {
     tiltPos--;
     tilt.write(tiltPos);
@@ -63,7 +67,7 @@ void loop() {
   }
 
   //Check if right button is pressed
-  if (rightVal==LOW && panPos < 180)
+  if (/*rightVal==LOW*/analogRead(X_pin)<250 && panPos < 180)
   {
     panPos++;
     pan.write(panPos);
@@ -71,7 +75,7 @@ void loop() {
   }
   
   //or if left button is pressed
-  else if (leftVal==LOW && panPos > 0)
+  else if (/*leftVal==LOW*/analogRead(X_pin)>450 && panPos > 0)
   {
     panPos--;
     pan.write(panPos);
